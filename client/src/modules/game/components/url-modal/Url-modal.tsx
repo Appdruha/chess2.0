1d53493c5fee7a2833ebe7df80463abb28669b1c
Appdruha <121567209+Appdruha@users.noreply.github.com>
2024-05-28 17:20:47 +0500

    улучшение ui

--- client/src/modules/game/components/url-modal/Url-modal.tsx
+++ client/src/modules/game/components/url-modal/Url-modal.tsx
@@ -1,14 +1,23 @@
+import CopyToClipboard from 'react-copy-to-clipboard'
 import styles from './url-modal.module.css'
+import { useState } from 'react'
 
 interface UrlModalProps {
   url: string
 }
 
 export const UrlModal = ({url}: UrlModalProps) => {
+  const [isCopied, setIsCopied] = useState(false)
+
   return (
     <div className={styles.container}>
       <h2 className={styles.heading}>Ссылка для подключения</h2>
-      <p>{url}</p>
+      <CopyToClipboard text={url} onCopy={() => setIsCopied(true)}>
+        <input className={styles.input} value={url}></input>
+      </CopyToClipboard>
+      <CopyToClipboard text={url} onCopy={() => setIsCopied(true)}>
+        <div className={styles.copy}>{isCopied ? 'Скопировано' : 'Скопировать'}</div>
+      </CopyToClipboard>
     </div>
   )
 }
